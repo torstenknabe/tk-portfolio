3efa137b382fd650f8206958acee8a1a57483a1c
Torsten Knabe <torsten.knabe@gmail.com>
2021-05-28 16:19:18 -0400

    styling and other updates

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -8,16 +8,22 @@ const Page = createClass({
     const entry = this.props.entry;
 
     return html`
-      <main id="main">
-        <div class="hero-container">
-          <h1 class="title-text"><span class="text-holder">${entry.getIn(["data", "title"], null)}</span></h1>
-        </div>
-        <section class="base-section">
-          <div class="container">
-            ${this.props.widgetFor("body")}
+      <div class="int">
+        <div class="wrapper" id="wrapper">
+          <div class="main-content">
+            <main id="main">
+              <div class="hero-container">
+                <h1 class="title-text"><span class="text-holder">${entry.getIn(["data", "title"], null)}</span></h1>
+              </div>
+              <section class="base-section">
+                <div class="container">
+                  ${this.props.widgetFor("body")}
+                </div>
+              </section>
+            </main>
           </div>
-        </section>
-      </main>
+        </div>
+      </div>
     `;
   }
 });
